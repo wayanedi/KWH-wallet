56ca028e903dc62d50fd699efa1f91a86718c32f
Handi Hermawan <handihand53@gmail.com>
2019-10-26 10:51:28 +0700

    First program init + adding splash screen

---
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.kwh_wallet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kwh_wallet.controller.Login;
+
+public class MainActivity extends AppCompatActivity {
+    private int loadTime=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.launch_ui);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(MainActivity.this, Login.class);
+                startActivity(home);
+                finish();
+
+            }
+        },loadTime);
+    }
+}
---
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/Login.java
@@ -0,0 +1,6 @@
+package com.example.kwh_wallet.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+}
