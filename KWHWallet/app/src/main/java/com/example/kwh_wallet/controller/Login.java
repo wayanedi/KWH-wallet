107ab486c3fff7532b51ef48f70775e691b9957a
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-11-03 15:49:33 +0700

    add signup logic

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/Login.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/Login.java
@@ -4,20 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.kwh_wallet.MainActivity;
 import com.example.kwh_wallet.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     private TextView hyperlink;
     private Button btn;
 
+
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+
+
         hyperlink = findViewById(R.id.HyperlinkDaftar);
         hyperlink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +49,5 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-
 
 }
