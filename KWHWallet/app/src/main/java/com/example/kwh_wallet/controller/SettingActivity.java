9534a0dff3e3a6c11f264dff2cda365dddc4e813
Kukuh Nugroho <kukuh.nugroho@ti.ukdw.ac.id>
2019-11-09 15:24:14 +0700

    Adding Setting Activity and Slide animation

---
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.kwh_wallet.controller;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kwh_wallet.R;
+
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_activity);
+    }
+}
