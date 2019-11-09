77c48a6af14e6c6adab00077874581a0eec1e993
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-11-09 13:24:17 +0700

    view pager

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/MenuActivity.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/MenuActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.kwh_wallet.R;
 
@@ -13,5 +14,18 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.menu_activity);
+
+        loadFragment(new HomeFragment());
+    }
+
+    // method untuk load fragment yang sesuai
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
