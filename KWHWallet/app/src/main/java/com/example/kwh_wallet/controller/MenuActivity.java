b70cd93214de684e33a613e492a3246457541e87
Kukuh Nugroho <kukuh.nugroho@ti.ukdw.ac.id>
2019-11-09 15:51:06 +0700

    Adding slide animation to setting

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/MenuActivity.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/MenuActivity.java
@@ -1,17 +1,31 @@
 package com.example.kwh_wallet.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.kwh_wallet.R;
 
 public class MenuActivity extends AppCompatActivity {
+    ImageView setting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.menu_activity);
+        setting = findViewById(R.id.setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuActivity.this, SettingActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
     }
 }
