9c1c56762290182d3a49d882dc25a85093c9c130
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-11-09 13:41:10 +0700

    + button navigation

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/MenuActivity.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/MenuActivity.java
@@ -1,14 +1,17 @@
 package com.example.kwh_wallet.controller;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.kwh_wallet.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,11 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.menu_activity);
 
         loadFragment(new HomeFragment());
+
+        // inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
     // method untuk load fragment yang sesuai
@@ -27,5 +35,20 @@ public class MenuActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.list_history:
+                fragment = new HistoryFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
     }
 }
