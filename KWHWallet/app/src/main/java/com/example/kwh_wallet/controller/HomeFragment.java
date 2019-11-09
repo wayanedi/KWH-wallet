814d40d5d5016a9130de212a0c9473d7e3318cb2
Handi Hermawan <handihand53@gmail.com>
2019-11-09 14:18:59 +0700

    Merge remote-tracking branch 'origin/xml/QrCodeTopUp' into feature/viewPager

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/HomeFragment.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/HomeFragment.java
@@ -1,14 +1,18 @@
 package com.example.kwh_wallet.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.kwh_wallet.MainActivity;
 import com.example.kwh_wallet.R;
 
 public class HomeFragment extends Fragment {
@@ -17,6 +21,22 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
+        Button transfer = (Button) view.findViewById(R.id.transfer);
+        transfer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent trans=new Intent(v.getContext(), TransferActivity.class);
+                startActivity(trans);
+            }
+        });
+
         return view;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
 }
---
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/TransferActivity.java
@@ -0,0 +1,15 @@
+package com.example.kwh_wallet.controller;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kwh_wallet.R;
+
+public class TransferActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transfer_activity);
+    }
+}
