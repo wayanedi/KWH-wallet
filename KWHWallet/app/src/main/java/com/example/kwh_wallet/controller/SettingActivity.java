a56af8d2e9f5e65f1c525a7cc8e6d78fe19d3dd1
Kukuh Nugroho <kukuh.nugroho@ti.ukdw.ac.id>
2019-11-09 17:32:27 +0700

    Adding Edit Profile and Change Password + Alert Dialog

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/SettingActivity.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/SettingActivity.java
@@ -1,6 +1,10 @@
 package com.example.kwh_wallet.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +12,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.kwh_wallet.R;
 
 public class SettingActivity extends AppCompatActivity {
+    Button ubahPw;
+    Button editPrfl;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_activity);
+        ImageView back = findViewById(R.id.backBtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        ubahPw = findViewById(R.id.ubahPassword);
+        ubahPw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ubahPassword = new Intent(SettingActivity.this, PasswordChangeActivity.class);
+                startActivity(ubahPassword);
+            }
+        });
+        editPrfl = findViewById(R.id.editProfil);
+        editPrfl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ubahProfile = new Intent(SettingActivity.this, EditProfileActivity.class);
+                startActivity(ubahProfile);
+            }
+        });
     }
 }
--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/TopUpActivity.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/TopUpActivity.java
@@ -1,6 +1,8 @@
 package com.example.kwh_wallet.controller;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,11 @@ public class TopUpActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.topup_activity);
+        ImageView back = findViewById(R.id.backBtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
