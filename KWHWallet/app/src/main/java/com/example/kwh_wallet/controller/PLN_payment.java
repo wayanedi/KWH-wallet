aea1972fe68f6bc6dc03cd21f0abc21d79bc20cd
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-11-30 16:51:45 +0700

    pln pin

--- KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/PLN_payment.java
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/PLN_payment.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.icu.text.DecimalFormat;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +23,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.beautycoder.pflockscreen.PFFLockScreenConfiguration;
+import com.beautycoder.pflockscreen.fragments.PFLockScreenFragment;
 import com.example.kwh_wallet.R;
 import com.example.kwh_wallet.model.History;
 import com.example.kwh_wallet.model.User;
@@ -41,13 +44,13 @@ import java.util.List;
 
 public class PLN_payment extends AppCompatActivity {
     private Button next;
-    private List<String> nominalPembayaran = new ArrayList<String>();
-    private Spinner nominal;
-    private RadioGroup rg;
+
+
     private TextView nomorMeter;
     private int getNominal= 0;
     Dialog customDialog;
     int total;
+    boolean actionNext = false;
     String pembayaran;
 
     private FirebaseAuth firebaseAuth;
@@ -64,61 +67,87 @@ public class PLN_payment extends AppCompatActivity {
         setContentView(R.layout.pln_payment_activity);
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
-        getSaldo();
-        getNominalList();
+        //getSaldo();
 
-        rg = findViewById(R.id.radioGroup);
-        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton rb = findViewById(checkedId);
-                if(rb != null){
-                    pembayaran = String.valueOf(rb.getText());
-                }
-            }
-        });
+        PFLockScreenFragment fragment = new PFLockScreenFragment();
+        PFFLockScreenConfiguration.Builder builder = new PFFLockScreenConfiguration.Builder(this)
+                .setMode(PFFLockScreenConfiguration.MODE_AUTH)
+                .setTitle("masukan security code anda")
+                .setCodeLength(6);
+        fragment.setConfiguration(builder.build());
+        fragment.setEncodedPinCode("F4zJ69ixo2Q+tAKJ/J70p+dW/8OKxocLRjelwbanPZ1NADerRw/5JF1UBPG86Ng9sC0LEm3yDrjhyRgIkp6KFYtnglWC3qcCGFEURZ2gSjQ9plo5vm6K6zXMHteekubUU4CEmc122v+TbUVEy4brB/C9LdxOjLnHymKoR+XCUUKb3IqQU3HOA51/8dWP4kQXI8/7OHITD2+v4U8B/Bup9tOieQkrVuvzLqhLEya5Ws34Bm2+nEUeNme0qDXX4Xmrq44vLwphX9aKsLLbSSbfDFCdD1Wz7kX2fMasO28pZ1JAORKYG2d8WfVZOHQPqWp96VJOOWA0e3lava8dDHj10g==");
+        fragment.setLoginListener(mLoginListener);
 
-        nomorMeter = findViewById(R.id.nomorMeter);
-        customDialog = new Dialog(this);
-        next =  findViewById(R.id.BayarPln);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_view_pln, fragment).commit();
+
     }
 
     public void nextPembayaran(View v){
-        customDialog.setContentView(R.layout.confirm_pln_payment);
-        TextView nomorMeterCD = customDialog.findViewById(R.id.nomorMeter);
-        TextView namaPelangganCD = customDialog.findViewById(R.id.namapelanggan);
-        TextView periodeCD = customDialog.findViewById(R.id.periode);
-        TextView biayaTagihCD = customDialog.findViewById(R.id.biayatagihan);
-        Calendar c = Calendar.getInstance();
-        String[] monthName = {"January","February","March", "April", "May", "June", "July",
-                "August", "September", "October", "November",
-                "December"};
-        String month = monthName[c.get(Calendar.MONTH)];
 
-        nomorMeterCD.setText(nomorMeter.getText());
-        namaPelangganCD.setText(firebaseUser.getEmail());
-        periodeCD.setText(month);
-        biayaTagihCD.setText("Rp. "+String.valueOf(getNominal));
-        total = getNominal+2000;
-        Button batal = customDialog.findViewById(R.id.Batalkan);
-        batal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                customDialog.dismiss();
-            }
-        });
+        System.out.println("oke dongggg");
+        System.out.println("pin pln: " + user.getPin());
 
-        Button bayar = customDialog.findViewById(R.id.BayarPln);
-        bayar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateSaldo(current_saldo-Double.parseDouble(String.valueOf(total)), firebaseUser.getUid(), "-");
-                finish();
-            }
-        });
 
-        customDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        customDialog.show();
+//        final View coba = v;
+//
+//        Spinner mySpinner = (Spinner) findViewById(R.id.nomimal);
+//        String text = mySpinner.getSelectedItem().toString();
+//        System.out.println("spinner: " + text);
+//
+//        for(int i=0 ; i<text.toString().length() ; i++){
+//            char c = text.toString().charAt(i);
+//            if(Character.isDigit(c)){
+//                getNominal = (getNominal*10)+Integer.parseInt(String.valueOf(c));
+//            }
+//        }
+//
+//        customDialog = new Dialog(v.getContext());
+//        nomorMeter = findViewById(R.id.nomorMeter);
+//        customDialog.setContentView(R.layout.confirm_pln_payment);
+//        TextView nomorMeterCD = customDialog.findViewById(R.id.nomorMeter);
+//        TextView namaPelangganCD = customDialog.findViewById(R.id.namapelanggan);
+//        TextView periodeCD = customDialog.findViewById(R.id.periode);
+//        TextView biayaTagihCD = customDialog.findViewById(R.id.biayatagihan);
+//        Calendar c = Calendar.getInstance();
+//        String[] monthName = {"January","February","March", "April", "May", "June", "July",
+//                "August", "September", "October", "November",
+//                "December"};
+//        String month = monthName[c.get(Calendar.MONTH)];
+//
+//        nomorMeterCD.setText(nomorMeter.getText());
+//        namaPelangganCD.setText(firebaseUser.getEmail());
+//        periodeCD.setText(month);
+//        biayaTagihCD.setText("Rp. "+String.valueOf(getNominal));
+//        total = getNominal+2000;
+//        Button batal = customDialog.findViewById(R.id.Batalkan);
+//        batal.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                customDialog.dismiss();
+//                actionNext = false;
+//            }
+//        });
+//
+//        Button bayar = customDialog.findViewById(R.id.BayarPln);
+//        bayar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                customDialog.dismiss();
+//
+//
+//            }
+//        });
+//
+//        customDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        customDialog.show();
+//        System.out.println("tess dong");
+
+
+    }
+
+    private void checkPin(){
 
     }
 
@@ -130,10 +159,10 @@ public class PLN_payment extends AppCompatActivity {
             SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy'_'HH:mm:ss");
             System.out.println(formatter.format(calendar.getTime()));
             DatabaseReference mDatabase;
-            History history = new History(formatter.format(calendar.getTime()), "+ Rp. "+String.valueOf(total), "Transfer");
+            History history = new History(formatter.format(calendar.getTime()), stats+" Rp. "+String.valueOf(total), "PLN");
             mDatabase = FirebaseDatabase.getInstance().getReference("history");
             mDatabase.child(key).child(formatter.format(calendar.getTime())).setValue(history);
-            Toast.makeText(getApplication(), "Transfer Berhasil", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplication(), "pembayaran Berhasil", Toast.LENGTH_SHORT).show();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -144,42 +173,26 @@ public class PLN_payment extends AppCompatActivity {
         finish();
     }
 
-    private void getNominalList(){
-        nominalPembayaran.add("Pilih Nominal");
-        nominalPembayaran.add("Rp 20.000");
-        nominalPembayaran.add("Rp 50.000");
-        nominalPembayaran.add("Rp 100.000");
-        nominalPembayaran.add("Rp 200.000");
-        nominalPembayaran.add("Rp 500.000");
-        nominalPembayaran.add("Rp 1.000.000");
-        nominalPembayaran.add("Rp 2.000.000");
+//    private void getNominalList(){
 
-        ArrayAdapter<String> nominalAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, nominalPembayaran);
-        nominal = findViewById(R.id.nomimal);
-        nominal.setAdapter(nominalAdapter);
-        nominal.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.colorOrangeDark));
-                Object item = parent.getItemAtPosition(position);
-                if(item != null){
-                    Toast.makeText(PLN_payment.this, item.toString(), Toast.LENGTH_SHORT).show();
-                    for(int i=0 ; i<item.toString().length() ; i++){
-                        char c = item.toString().charAt(i);
-                        if(Character.isDigit(c)){
-                            getNominal = (getNominal*10)+Integer.parseInt(String.valueOf(c));
-                        }
-                    }
+//        nominal.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.colorOrangeDark));
+//                Object item = parent.getItemAtPosition(position);
+//                if(item != null){
+//                    Toast.makeText(PLN_payment.this, item.toString(), Toast.LENGTH_SHORT).show();
 
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//    }
 
     private void getSaldo(){
         Query query = FirebaseDatabase.getInstance().getReference("users")
@@ -196,15 +209,26 @@ public class PLN_payment extends AppCompatActivity {
 
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     user = snapshot.getValue(User.class);
-                    sisaSaldo = findViewById(R.id.sisaSaldoLabel);
-                    DecimalFormat df = new DecimalFormat("#");
-//                    System.out.print();
 
-                    sisaSaldo.setText("Sisa Saldo Anda Rp. "+String.format("%,.0f", user.getSaldo()));
-                    sisaSaldo.setVisibility(View.VISIBLE);
-                    sisaSaldo.bringToFront();
+
                     System.out.println("saldo user: " + user.getSaldo());
                     current_saldo=user.getSaldo();
+
+
+
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            Bundle bundle = new Bundle();
+//
+//                            Pln_payment_fragment fragInfo = new Pln_payment_fragment();
+//                            bundle.putDouble("saldo", current_saldo);
+//                            fragInfo.setArguments(bundle);
+//
+//                            getSupportFragmentManager().beginTransaction()
+//                                    .replace(R.id.container_view_pln, fragInfo).commit();
+//                        }
+//                    },1000);
                 }
 
             }
@@ -215,4 +239,28 @@ public class PLN_payment extends AppCompatActivity {
 
         }
     };
+
+    private final  PFLockScreenFragment.OnPFLockScreenLoginListener mLoginListener =
+            new PFLockScreenFragment.OnPFLockScreenLoginListener() {
+                @Override
+                public void onCodeInputSuccessful() {
+                    System.out.println("pin sama");
+                }
+
+                @Override
+                public void onFingerprintSuccessful() {
+
+                }
+
+                @Override
+                public void onPinLoginFailed() {
+
+                }
+
+                @Override
+                public void onFingerprintLoginFailed() {
+
+                }
+            };
+
 }
---
+++ KWHWallet/app/src/main/java/com/example/kwh_wallet/controller/Pln_payment_fragment.java
@@ -0,0 +1,67 @@
+package com.example.kwh_wallet.controller;
+
+import android.icu.text.DecimalFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.kwh_wallet.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pln_payment_fragment extends Fragment {
+
+    private TextView sisaSaldo;
+    private Spinner nominal;
+    private List<String> nominalPembayaran = new ArrayList<String>();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.pln_payment_fragment, container, false);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Bundle bundle = getArguments();
+                double saldo = bundle.getDouble("saldo");
+                System.out.println("saldo di plm: " + saldo);
+
+                sisaSaldo = view.findViewById(R.id.sisaSaldoLabel);
+                DecimalFormat df = new DecimalFormat("#");
+                sisaSaldo.setText("Sisa Saldo Anda Rp. "+String.format("%,.0f", saldo));
+                sisaSaldo.setVisibility(View.VISIBLE);
+                sisaSaldo.bringToFront();
+
+                nominalPembayaran.add("Pilih Nominal");
+                nominalPembayaran.add("Rp 20.000");
+                nominalPembayaran.add("Rp 50.000");
+                nominalPembayaran.add("Rp 100.000");
+                nominalPembayaran.add("Rp 200.000");
+                nominalPembayaran.add("Rp 500.000");
+                nominalPembayaran.add("Rp 1.000.000");
+                nominalPembayaran.add("Rp 2.000.000");
+
+                ArrayAdapter<String> nominalAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, nominalPembayaran);
+                nominal = view.findViewById(R.id.nomimal);
+                nominal.setAdapter(nominalAdapter);
+            }
+        },1000);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
